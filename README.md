8f8c2d180e8995d6b70e1bf80e9f96cb567478c5
omolade sunday <sundayomoladee11@gmail.com>
2023-01-19 23:34:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+A movies web app, where you can search for movies and get more details about it
+
+installation
+
+make sure node is installed on your machine
+
+git clone https://github.com/OMOLADESUNDAY/React-video-web-app.git
+
+npm install
+
+check it out
+
+https://profound-bubblegum-1db428.netlify.app
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Header from './component/Header';
+import Navbar from './component/Navbar';
+import Movies from './component/Movies';
+import Newsletter from './component/Newsletter'
+import Footer from './component/Footer'
+import {Routes,Route} from 'react-router-dom';
+import Login from './component/Login';
+function App() {
+  return (
+    <div className="">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Header/>}/>
+        <Route path='/user' element={<Login/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/newsletter' element={<Newsletter/>}/>
+      </Routes>
+
+  
+    
+      <Footer/>
+    
+    </div>
+  );
+}
+
+export default App;
